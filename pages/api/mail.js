b8ab144112d1b0361475e75be102729607326a5a
James Hagood <cardfaux@tutanota.com>
2022-04-20 08:38:11 -0500

    added a res.end()

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,27 +1,41 @@
+import { resolveHref } from 'next/dist/shared/lib/router/router';
+
 const mail = require('@sendgrid/mail');
 
 mail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default (req, res) => {
-  const stringFix = JSON.stringify(req.body);
-  const body = JSON.parse(stringFix);
-  // const body = JSON.parse(req.body);
+  if (req.method === 'POST') {
+    const stringFix = JSON.stringify(req.body);
+    const body = JSON.parse(stringFix);
+    // const body = JSON.parse(req.body);
 
-  const message = `
+    const message = `
   Name: ${body.name}\r\n
   Email: ${body.email}\r\n
   Message: ${body.message}
   `;
 
-  const data = {
-    to: 'studiofromtheashes.studio@gmail.com',
-    from: 'hello@fromtheashes.studio',
-    subject: 'New web form message!',
-    text: message,
-    html: message.replace(/\r\n/g, '<br>'),
-  };
+    const data = {
+      to: 'studiofromtheashes.studio@gmail.com',
+      from: 'hello@fromtheashes.studio',
+      subject: 'New web form message!',
+      text: message,
+      html: message.replace(/\r\n/g, '<br>'),
+    };
 
-  mail.send(data);
-
-  res.status(200).json({ status: 'Ok' });
+    mail
+      .send(data)
+      .then(() => {
+        res.status(200).json({ status: 'Ok' });
+        res.end();
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(error).json({ status: 'something went wrong' });
+      });
+  } else {
+    res.status(405);
+    res.end();
+  }
 };
