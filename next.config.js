5144963ba371004406901adbcc017d7cf5d01edb
James Hagood <cardfaux@tutanota.com>
2022-04-20 07:43:03 -0500

    trying to fix contact form

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ module.exports = {
     SANITY_DATASET_NAME: process.env.SANITY_DATASET_NAME,
     SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
     GOOGLE_ANALYTICS_ID: process.env.GOOGLE_ANALYTICS_ID,
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
   },
   nextConfig,
 };
